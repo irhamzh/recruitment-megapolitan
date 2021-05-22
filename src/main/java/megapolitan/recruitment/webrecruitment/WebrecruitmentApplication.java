53252ed410ddb152d4fc22b2336f44f087add0a0
Irham Ilman Zhafir <irhamz175@gmail.com>
2021-05-22 20:06:41 +0700

    init push

---
+++ src/main/java/megapolitan/recruitment/webrecruitment/WebrecruitmentApplication.java
@@ -0,0 +1,13 @@
+package megapolitan.recruitment.webrecruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebrecruitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebrecruitmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/megapolitan/recruitment/webrecruitment/WebrecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package megapolitan.recruitment.webrecruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrecruitmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
